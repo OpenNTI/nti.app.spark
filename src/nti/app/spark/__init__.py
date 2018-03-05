2f8ed76c8cfcf264609460ec62c007ac96f12183
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-05 09:38:48 -0600

    add func to get job factory

--- src/nti/app/spark/__init__.py
+++ src/nti/app/spark/__init__.py
@@ -8,6 +8,10 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
+from zope import component
+
+from nti.app.spark.interfaces import ISparkJobQueueFactory
+
 #: Spark job NTIID Type
 SPARK_JOB = u'SparkJob'
 
@@ -15,3 +19,9 @@ SPARK_JOB = u'SparkJob'
 SPARK_JOBS_QUEUE = '++etc++spark++queue++jobs'
 
 QUEUE_NAMES = (SPARK_JOBS_QUEUE,)
+
+logger = __import__('logging').getLogger(__name__)
+
+
+def get_factory():
+    return component.getUtility(ISparkJobQueueFactory)
