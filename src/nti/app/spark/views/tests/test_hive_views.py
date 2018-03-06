0c06b83e2b8765dc399e14e873e03bd6facc6dc7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-06 10:58:12 -0600

    add view to get hive tables

--- src/nti/app/spark/views/tests/test_hive_views.py
+++ src/nti/app/spark/views/tests/test_hive_views.py
@@ -7,6 +7,10 @@ from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods,arguments-differ
 
+from hamcrest import has_length
+from hamcrest import has_entries
+from hamcrest import assert_that
+from hamcrest import greater_than
 
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
@@ -18,3 +22,11 @@ class TestSparkViews(ApplicationLayerTest):
     @WithSharedApplicationMockDS(testapp=True, users=True)
     def test_spark_403(self):
         self.testapp.get('/dataserver2/spark', status=403)
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_spark_tables(self):
+        res = self.testapp.get('/dataserver2/spark/hive/@@tables',
+                               status=200)
+        assert_that(res.json_body,
+                    has_entries('Items', has_length(greater_than(0)),
+                                'Total', greater_than(0)))
