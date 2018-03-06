c2a3874fc63c7a0a10c2ea8acc7a7981a081458f
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-06 10:57:57 -0600

    add ACL for IHiveTable objects

---
+++ src/nti/app/spark/acl.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from zope import component
+from zope import interface
+
+from zope.cachedescriptors.property import Lazy
+
+from nti.dataserver import authorization
+
+from nti.dataserver.authorization_acl import ace_allowing
+from nti.dataserver.authorization_acl import acl_from_aces
+
+from nti.dataserver.interfaces import ALL_PERMISSIONS
+
+from nti.dataserver.interfaces import IACLProvider
+
+from nti.spark.interfaces import IHiveTable
+
+logger = __import__('logging').getLogger(__name__)
+
+
+@interface.implementer(IACLProvider)
+class _ACLProviderMixin(object):
+
+    def __init__(self, context):
+        self.context = context
+
+    @Lazy
+    def __acl__(self):
+        aces = [ace_allowing(authorization.ROLE_ADMIN, ALL_PERMISSIONS, type(self))]
+        acl = acl_from_aces(aces)
+        return acl
+
+
+@component.adapter(IHiveTable)
+class _HiveTableACLProvider(_ACLProviderMixin):
+    pass
