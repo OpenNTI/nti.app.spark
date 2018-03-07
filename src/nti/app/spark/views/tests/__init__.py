ce00e4b3903fdc1c73e2d3ef120007812819c703
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-06 21:00:31 -0600

    add application test layer

--- src/nti/app/spark/views/tests/__init__.py
+++ src/nti/app/spark/views/tests/__init__.py
@@ -6,3 +6,38 @@ from __future__ import print_function
 from __future__ import absolute_import
 
 # pylint: disable=protected-access,too-many-public-methods,arguments-differ
+
+import os
+import shutil
+
+from zope import component
+
+from nti.app.testing.application_webtest import ApplicationTestLayer
+
+from nti.spark.interfaces import IHiveSparkInstance
+
+
+class SparkApplicationTestLayer(ApplicationTestLayer):
+
+    @classmethod
+    def clean_up(cls):
+        shutil.rmtree(os.path.join(os.getcwd(), 'home'), True)
+        shutil.rmtree(os.path.join(os.getcwd(), 'metastore_db'), True)
+        shutil.rmtree(os.path.join(os.getcwd(), 'spark-warehouse'), True)
+
+    @classmethod
+    def setUp(cls):
+        pass
+
+    @classmethod
+    def tearDown(cls):
+        component.getUtility(IHiveSparkInstance).close()
+        cls.clean_up()
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        pass
+
+    @classmethod
+    def testTearDown(cls, test=None):
+        pass
