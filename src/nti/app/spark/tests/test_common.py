939360b5e16524dabaacfccd6567eb84441997f2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-15 15:53:18 -0500

    test funcs

--- src/nti/app/spark/tests/test_common.py
+++ src/nti/app/spark/tests/test_common.py
@@ -9,11 +9,18 @@ from __future__ import absolute_import
 
 from hamcrest import is_
 from hamcrest import none
+from hamcrest import is_not
 from hamcrest import assert_that
 
+import time
 import unittest
+from datetime import datetime
+
+import fudge
 
 from nti.app.spark.common import get_site
+from nti.app.spark.common import get_redis_lock
+from nti.app.spark.common import parse_timestamp
 
 from nti.app.spark.tests import SharedConfiguringTestLayer
 
@@ -24,3 +31,12 @@ class TestCommon(unittest.TestCase):
 
     def test_get_site(self):
         assert_that(get_site(None, object()), is_(none()))
+
+    def test_parse_timestamp(self):
+        assert_that(parse_timestamp(time.time()), is_(datetime))
+        assert_that(parse_timestamp('2017-11-30'), is_(datetime))
+
+    @fudge.patch('nti.app.spark.common.RedisLock')
+    def test_get_redis_lock(self, mock_rl):
+        mock_rl.is_callable().returns_fake()
+        assert_that(get_redis_lock('foo'), is_not(none()))
