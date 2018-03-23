d6b9b4ea069f75d936e2d97cf2757a667456327d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-22 20:48:35 -0500

    add deps

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         'nti.coremetadata',
         'nti.dublincore',
         'nti.externalization',
+        'nti.links',
         'nti.ntiids',
         'nti.property',
         'nti.schema',
