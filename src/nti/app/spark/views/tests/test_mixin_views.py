2f958a25a74b82e7da5840dd8df380fd5d66454d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-03-27 14:21:59 -0500

    return a job for a single source

--- src/nti/app/spark/views/tests/test_mixin_views.py
+++ src/nti/app/spark/views/tests/test_mixin_views.py
@@ -38,7 +38,7 @@ class TestMixinViews(unittest.TestCase):
             name: source
         })
         mock_mfl.is_callable().returns(0)
-        mock_upj.is_callable().with_args().returns('job')
+        mock_upj.is_callable().with_args().returns({'jobId': 'myId'})
 
         request = fudge.Fake()
         request.has_attr(view_name="name").has_attr(context=None)
@@ -49,10 +49,10 @@ class TestMixinViews(unittest.TestCase):
         mock_mfl.is_callable().returns(100)
         result = view.do_call("creator",  100, True)
         assert_that(result,
-                    has_entries('Items', has_entries(name, 'job')))
+                    has_entries('jobId', 'myId'))
 
     def test_coverage(self):
         view = AbstractHiveUploadView(None)
         assert_that(view.max_file_length(), is_(DEFAULT_MAX_SOURCE_SIZE))
         with self.assertRaises(NotImplementedError):
-            view.create_upload_job("creator", "target", 100, True)
+            view.create_upload_job("creator", "target", 100, True, False)
