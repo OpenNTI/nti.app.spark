f47f0d619a93d35179025aba0c98111131c636fc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-04-13 13:26:25 -0500

    remove unused import

--- src/nti/app/spark/views/__init__.py
+++ src/nti/app/spark/views/__init__.py
@@ -8,8 +8,6 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-from pyramid import httpexceptions as hexc
-
 from zope import component
 from zope import interface
 
